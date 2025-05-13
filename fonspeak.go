3da30a6a5ca4d48c4a634ed9ffcc4dccbc0eb39c
Sammy Shear <sammyshear1@gmail.com>
2025-05-13 02:51:27 -0400

    embed pitch shift in library

--- fonspeak.go
+++ fonspeak.go
@@ -23,8 +23,9 @@ type PhraseParams struct {
 }
 
 func pitchShift(wave string, shift float64) error {
-	cmd := exec.Command("python/pitch_shifter.py", fmt.Sprint(shift), wave)
-	out, err := cmd.Output()
+	cmd := exec.Command("python", "-c", fmt.Sprintf(`import librosa;import psola;import argparse;import soundfile as sf;from pathlib import Path;print('test');filepath = Path('%s');y, sr = librosa.load(str(filepath), sr=None, mono=False);fmin = librosa.note_to_hz('C1').astype(float);fmax = librosa.note_to_hz('C7').astype(float);corrected_f0 = [%f];pitch_corrected_y = psola.vocode(y, sample_rate=int(sr), target_pitch=corrected_f0, fmin=fmin, fmax=fmax);sf.write(str(filepath), pitch_corrected_y, sr);`, wave, shift))
+	fmt.Printf("%v\n", cmd.Args)
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return err
 	}
