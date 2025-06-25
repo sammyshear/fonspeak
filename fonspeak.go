6fcea0d58f781b4450841889be739322d5d85f01
Sammy Shear <sammyshear1@gmail.com>
2025-06-25 18:49:37 -0400

    pure golang

--- fonspeak.go
+++ fonspeak.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"os/exec"
 	"sync"
+
+	"github.com/go-audio/wav"
 )
 
 type SyllableResult struct {
@@ -31,13 +34,49 @@ type PhraseParams struct {
 }
 
 func pitchShift(wave string, shift float64) error {
-	cmd := exec.Command("python", "-c", fmt.Sprintf(`import librosa;import psola;import argparse;import soundfile as sf;from pathlib import Path;filepath = Path('%s');y, sr = librosa.load(str(filepath), sr=None, mono=False);fmin = librosa.note_to_hz('C1').astype(float);fmax = librosa.note_to_hz('C7').astype(float);corrected_f0 = [%f];pitch_corrected_y = psola.vocode(y, sample_rate=int(sr), target_pitch=corrected_f0, fmin=fmin, fmax=fmax);sf.write(str(filepath), pitch_corrected_y, sr);`, wave, shift))
-	out, err := cmd.CombinedOutput()
+	waveout := wave + "_out.wav"
+	f, err := os.Open(wave)
 	if err != nil {
 		return err
 	}
 
-	fmt.Print(string(out))
+	defer f.Close()
+
+	decoder := wav.NewDecoder(f)
+	buf, err := decoder.FullPCMBuffer()
+	if err != nil {
+		return err
+	}
+
+	var audioData []float64
+
+	numSamples := len(buf.Data) / buf.Format.NumChannels
+	audioData = make([]float64, numSamples)
+
+	for i := range numSamples {
+		// For mono, or just taking the first channel of stereo
+		audioData[i] = float64(buf.Data[i*buf.Format.NumChannels]) / math.Pow(2, float64(buf.SourceBitDepth-1)-1)
+	}
+	fmt.Printf("Read %d samples from WAV file.\n", len(audioData))
+
+	originalF0, err := estimateFundamentalFrequency(audioData, int(decoder.SampleRate))
+	if err != nil {
+		return fmt.Errorf("error estimating original F0: %v", err)
+	}
+
+	fmt.Printf("Estimated original F0: %.2f Hz\n", originalF0)
+
+	pitchRatio := shift / originalF0
+
+	fmt.Printf("PitchRatio: %.2f\n", pitchRatio)
+
+	cmd := exec.CommandContext(context.Background(), "rubberband-r3", "-t", "1.0", "-p", fmt.Sprintf("%.1f", pitchRatio), wave, waveout)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	if err := os.Rename(waveout, wave); err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -65,7 +104,7 @@ func FonspeakPhrase(params PhraseParams, grMax int) error {
 		return err
 	}
 
-	defer os.RemoveAll(dir)
+	// defer os.RemoveAll(dir)
 
 	var waves []string
 
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/sammyshear/fonspeak
 
 go 1.24.3
+
+require (
+	github.com/go-audio/audio v1.0.0 // indirect
+	github.com/go-audio/riff v1.0.0 // indirect
+)
+
+require (
+	github.com/go-audio/wav v1.1.0
+	gonum.org/v1/gonum v0.16.0
+)
